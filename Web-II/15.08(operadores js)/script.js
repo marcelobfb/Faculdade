d3c5d3dc7f26bc76d8b5cf355304d558ad73ac0b
marcelobfb <145417804+marcelobfb@users.noreply.github.com>
2024-08-15 09:18:46 -0300

    up

--- Web-II/15.08(operadores js)/script.js
+++ Web-II/15.08(operadores js)/script.js
@@ -1,27 +1,34 @@
 //operadores de atribuição
-var a = Number(prompt("Digite um numero"));
-a += 5;
-alert("a soma dos valores = " + a);
-document.write("a soma dos valores = " + a);
-console.log("a soma dos valores = " + a);
-//
-var b = Number(prompt("digite um numero"));
-b -= a;
-alert("a subtração dos valores = " + b);
-document.write("<br>a subtração dos valores= " + b);
-console.log("a subtração dos valores = " + b);
-//
-alert(`A soma dos valores = ${a}`);
-document.write(`<br>A soma dos valores =${a}`);
-console.log(`A soma dos valores = ${a}`);
-//
-a *= b;
-alert(`A multiplicação dos valores = ${a}`);
-document.write(`<br>A multiplicação dos valores =${a}`);
-console.log(`a multiplicação dos valores = ${a}`);
-//
-var a1 = Number(prompt("Digite um numero "));
-var b1 = Number(prompt("Digite um numero "));
-alert(`A soma de ${a1} + ${b1} = ${a1 + b1}`);
-document.write(`<br>A soma de ${a1} + ${b1} = ${a1 + b1}`);
-console.log(`a soma de ${a1}+${b1}=${a1 + b1}`);
+// var a = Number(prompt("Digite um numero"));
+// a += 5;
+// alert("a soma dos valores = " + a);
+// document.write("a soma dos valores = " + a);
+// console.log("a soma dos valores = " + a);
+
+// var b = Number(prompt("digite um numero"));
+// b -= a;
+// alert("a subtração dos valores = " + b);
+// document.write("<br>a subtração dos valores= " + b);
+// console.log("a subtração dos valores = " + b);
+
+// alert(`A soma dos valores = ${a}`);
+// document.write(`<br>A soma dos valores =${a}`);
+// console.log(`A soma dos valores = ${a}`);
+
+// a *= b;
+// alert(`A multiplicação dos valores = ${a}`);
+// document.write(`<br>A multiplicação dos valores =${a}`);
+// console.log(`a multiplicação dos valores = ${a}`);
+
+// var a1 = Number(prompt("Digite um numero "));
+// var b1 = Number(prompt("Digite um numero "));
+// alert(`A soma de ${a1} + ${b1} = ${a1 + b1}`);
+// document.write(`<br>A soma de ${a1} + ${b1} = ${a1 + b1}`);
+// console.log(`a soma de ${a1}+${b1}=${a1 + b1}`);
+//incremento(++) e decremento(--)
+num1=20
+document.write(num1++)//pos incrementa, incrementa mas n mostra o valor incrementado
+document.write(`<br> ${num1}`)
+document.write(`<br> ${++num1}`)//pre incremento, incrementa e mostra o valor incrementado
+
+
