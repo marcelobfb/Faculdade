ca21efadd6ee860c1a93f3e6dd19c1c181b6e05c
marcelobfb <145417804+marcelobfb@users.noreply.github.com>
2024-08-15 09:06:40 -0300

    up

--- Web-II/15.08(operadores js)/script.js
+++ Web-II/15.08(operadores js)/script.js
@@ -1,16 +1,27 @@
 //operadores de atribuição
-var a = number(prompt("Digite um numero"));
+var a = Number(prompt("Digite um numero"));
 a += 5;
 alert("a soma dos valores = " + a);
 document.write("a soma dos valores = " + a);
 console.log("a soma dos valores = " + a);
 //
-var b = number(prompt("digite um numero"));
+var b = Number(prompt("digite um numero"));
 b -= a;
 alert("a subtração dos valores = " + b);
 document.write("<br>a subtração dos valores= " + b);
 console.log("a subtração dos valores = " + b);
 //
-alert();
-document.write();
-console.log();
+alert(`A soma dos valores = ${a}`);
+document.write(`<br>A soma dos valores =${a}`);
+console.log(`A soma dos valores = ${a}`);
+//
+a *= b;
+alert(`A multiplicação dos valores = ${a}`);
+document.write(`<br>A multiplicação dos valores =${a}`);
+console.log(`a multiplicação dos valores = ${a}`);
+//
+var a1 = Number(prompt("Digite um numero "));
+var b1 = Number(prompt("Digite um numero "));
+alert(`A soma de ${a1} + ${b1} = ${a1 + b1}`);
+document.write(`<br>A soma de ${a1} + ${b1} = ${a1 + b1}`);
+console.log(`a soma de ${a1}+${b1}=${a1 + b1}`);
