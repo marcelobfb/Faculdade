4ffeadd9b0c6553ea5f8f33a2f9fbcf95f38854a
marcelobfb <145417804+marcelobfb@users.noreply.github.com>
2024-08-15 09:42:52 -0300

    up

--- Web-II/15.08(operadores js)/script.js
+++ Web-II/15.08(operadores js)/script.js
@@ -1,4 +1,4 @@
-//operadores de atribuição
+//*operadores de atribuição
 // var a = Number(prompt("Digite um numero"));
 // a += 5;
 // alert("a soma dos valores = " + a);
@@ -25,10 +25,27 @@
 // alert(`A soma de ${a1} + ${b1} = ${a1 + b1}`);
 // document.write(`<br>A soma de ${a1} + ${b1} = ${a1 + b1}`);
 // console.log(`a soma de ${a1}+${b1}=${a1 + b1}`);
-//incremento(++) e decremento(--)
-num1=20
-document.write(num1++)//pos incrementa, incrementa mas n mostra o valor incrementado
-document.write(`<br> ${num1}`)
-document.write(`<br> ${++num1}`)//pre incremento, incrementa e mostra o valor incrementado
 
+//*incremento(++) e decremento(--)
+// num1=20
+// document.write(num1++)//pos incrementa, incrementa mas n mostra o valor incrementado
+// document.write(`<br> ${num1}`)
+// document.write(`<br> ${++num1}`)//pre incremento, incrementa e mostra o valor incrementado
+// document.write(`<br> O valor de ${num1} incrementado é igual a ${++num1}`)
+// num2=10
+// document.write(`<br>${num2--}`)
+// document.write(`<br>${num2}`)
+// document.write(`<br>${--num2}`)
+// document.write(`<br> O valor de ${num2} decrementado é igual a ${--num2}`)
 
+//*operadores de comparação ==, ===
+//== --> verifica se os dados são iguais
+//=== --> verifica se os valores são iguais
+x=10
+y='10'
+document.write(`O valor de ${x} é igual o valor de ${y} ? ${x==y}`)
+document.write(`<br> O valor de ${x} é igual o valor de ${y} ? ${x===y}`)
+x1=+(prompt('Digite um numero'))
+y1=prompt('Digite um numero')
+document.write(`<br>O valor ${x1} é igual ${y1}? ${x1==y1}`)
+document.write(`<br>O valor ${x1} é igual ${y1}? ${x1===y1}`)
