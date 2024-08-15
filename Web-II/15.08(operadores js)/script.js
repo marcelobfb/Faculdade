c7c6db1cce0b05d72706c83642eb5f13c0a921c1
marcelobfb <145417804+marcelobfb@users.noreply.github.com>
2024-08-15 08:50:08 -0300

    up

--- Web-II/15.08(operadores js)/script.js
+++ Web-II/15.08(operadores js)/script.js
@@ -1,1 +1,16 @@
 //operadores de atribuição
+var a = number(prompt("Digite um numero"));
+a += 5;
+alert("a soma dos valores = " + a);
+document.write("a soma dos valores = " + a);
+console.log("a soma dos valores = " + a);
+//
+var b = number(prompt("digite um numero"));
+b -= a;
+alert("a subtração dos valores = " + b);
+document.write("<br>a subtração dos valores= " + b);
+console.log("a subtração dos valores = " + b);
+//
+alert();
+document.write();
+console.log();
