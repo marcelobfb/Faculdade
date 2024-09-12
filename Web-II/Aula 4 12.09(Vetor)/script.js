b859a2cece9c858925b11fbec66c60d04fb560ec
marcelobfb <marcelobouchardet@hotmail.com>
2024-09-12 11:43:25 -0300

    up

--- Web-II/Aula 4 12.09(Vetor)/script.js
+++ Web-II/Aula 4 12.09(Vetor)/script.js
@@ -61,6 +61,79 @@
 // document.write(`<br>O numero 2 encontra-se no array? ${(f.includes(2))}`);
 // document.write(`<br>O numero 2 encontra-se no array? ${f.includes(12)}`);
 //*push()- adiciona um novo elemento no final do array
-var i=[10,20,30,40]
-i.push('azul')
-document.write(`<br>${i}`)
+// var i = [10, 20, 30, 40];
+// i.push("azul");
+// document.write(`<br>${i}`);
+//*pop() - remove o ultimo elemento do vetor
+// i.pop();
+// document.write(`<br>${i}`);
+//*unshift() - adicionar novo elemento no inicio do vetor
+// i.unshift("azul");
+// document.write(`<br>${i}`);
+//*shift() - remover o primeiro elemento
+// i.shift();
+// document.write(`<br>${i}`);
+//*slice() - fatia o array sem alterar o array original
+// document.write(`<br>${i.slice(2)}`);
+// document.write(`<br>${i.slice(0, 2)}`);
+// letras = ["a", "b", "c", "d", "f", "g"];
+// c = letras.slice(2);
+// document.write(`<br>${c}`);
+// c1 = letras.slice(2, 3);
+// document.write(`<br>${c1}`);
+// c2 = letras.slice(2, 5);
+// document.write(`<br>${c2}`);
+// c3 = letras.slice(-4, -2);
+// document.write(`<br>${c3}`);
+// c4 = letras.slice(-6, -3);
+// document.write(`<br>${c4}`);
+
+// email = "exemplo@gmail.com";
+// e = email.slice(email.indexOf("@") + 1);
+// document.write(`<br>${e}`);
+
+// e2 = email.slice(email.lastIndexOf("m") + 1);
+// document.write(`<br>${e2}`);
+
+// e1 = email.slice(email.lastIndexOf("e"));
+// document.write(`<br>${e1}`);
+
+// nome2='Thereza'
+// n1=nome2.slice(-3)
+// document.write(`<br>${n1}`)
+
+//*splice()- 1º valor: posição, 2º valor: quantidade de elementos excluidos, 3º valor: novos elementos
+
+// d=['segunda','terça','quarta']
+// d1=d.splice(1,1)
+// document.write(`<br>${d1}`)
+// document.write(`<br>${d}`)
+
+// d2=d.sploce(1,1,'quinta','sexta')
+// document.write(`<br>${d2}`)
+// document.write(`<br>${d}`)
+// d=['segunda','terça','quarta']
+// d1=d.splice(1,1)
+// document.write(`<br>${d}`)
+
+// nome=[]
+// for(i=0;i<5;i++){
+//     nome[i]=prompt('Digite o seu nome: ')
+// }
+// document.write(`<br>Nome: ${nome}`)
+// n=nome.splice(2,0,'1')
+// document.write(`<br>Nome: ${nome}`)
+
+//*funcao anonima - n possui nome, atribui-se a funca
+var msg=function(x){
+    return `ola ${x} bom dia`
+}
+nome=prompt('nome: ')
+nome=nome[0].toUpperCase()+nome.substring(1)
+document.write(`<br>${msg(nome)}`)
+
+num=function(x){
+    return x**2
+}
+numero=Number(prompt('Digite numero: '))
+document.write(`<br>${num(numero)}`)
