9b0820c20904fb8e09e44b479dc1c52c36f8d12c
marcelobfb <145417804+marcelobfb@users.noreply.github.com>
2025-02-12 21:27:47 -0300

    aula 2

---
+++ web-III/Aula 1 (05/02/25)/main.py
@@ -0,0 +1,37 @@
+# TDE1
+
+# 1. Peça ao usuário um número inteiro e calcule o dobro desse número.
+# num = int(input("Digite um número inteiro: "))
+# dobro = num * 2
+# print(dobro)
+
+#2. Solicite a largura e a altura de um retângulo e calcule a área.
+# largura = float(input("Digite a largura do retângulo: "))
+# altura = float(input("Digite a altura do retângulo: "))
+# area = largura * altura
+# print(area)
+
+#3. Peça dois números inteiros e use o operador de igualdade (==) para verificar se eles são iguais.
+# num1 = int(input("Digite um número inteiro: "))
+# num2 = int(input("Digite outro número inteiro: "))
+# iguais = num1 == num2
+# print(iguais)
+
+#4. Peça dois números inteiros e calcule o resto da divisão entre eles usando o operador módulo (%).
+# num1 = int(input("Digite um número inteiro: "))
+# num2 = int(input("Digite outro número inteiro: "))
+# resto = num1 % num2
+# print(resto)
+
+#5. Solicite três números e calcule a média aritmética simples deles.
+# num1 = float(input("Digite um número: "))
+# num2 = float(input("Digite outro número: "))
+# num3 = float(input("Digite mais um número: "))
+# media = (num1 + num2 + num3) / 3
+# print(media)
+
+#6. Peça dois números e verifique se o primeiro é maior que 10 e o segundo é menor que 5 usando o operador and.
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+# verifica = num1 > 10 and num2 < 5
+# print(verifica)
