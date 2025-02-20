e5deb2d0cbc931e946dfd17d4271d7417397ba90
marcelobfb <marcelobouchardet@hotmail.com>
2025-02-19 21:50:41 -0300

    up

---
+++ web-III/aula 02.19/main.py
@@ -0,0 +1,13 @@
+# aula 3///
+# Estrutura condicionais
+# x=int(input("Número: "))
+# if x%2==0 and x!=0:
+#     print(f"O Número {x} é Par.")
+# elif x==0:
+#     print(f"Você digitou o Número {x}.")
+# else:
+#     print(f"O Número {x} é Ímpar.")
+    
+x=int(input("numero: "))
+for tabuada in range(1,11):
+    print(f"{x}x{tabuada}={x*tabuada}")
