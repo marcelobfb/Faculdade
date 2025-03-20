0ee5e7da74f9d1a31a1b562e2015208f4deac483
marcelobfb <marcelobouchardet@hotmail.com>
2025-03-20 19:59:30 -0300

    up

---
+++ prog III/aula5/src/controle/controlapessoa.java
@@ -0,0 +1,22 @@
+package controle;
+import dominio.*;
+import  java.util.Scanner;
+
+public class controlapessoa {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.printf("Digite a id: ");
+        int id=scanner.nextInt();
+        System.out.printf("Digite o nome: ");
+        String nome = scanner.next();
+        System.out.printf("Digite a idade: ");
+        int idade = scanner.nextInt();
+        System.out.printf("Digite o peso: ");
+        double peso=scanner.nextDouble();
+        System.out.printf("Digite a altura: ");
+        double altura=scanner.nextDouble();
+        pessoa pessoa=new pessoa(id,nome,idade,peso,altura);
+        pessoa.listarPessoa();
+        scanner.close();
+    }
+}
---
+++ prog III/aula5/src/dominio/pessoa.java
@@ -0,0 +1,65 @@
+package dominio;
+
+public class pessoa {
+    private int id;
+    private String nome;
+    private int idade;
+    private double peso;
+    private double altura;
+
+    public pessoa(int id, String nome, int idade, double peso, double altura) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    public void listarPessoa(){
+        System.out.println("\n"+id);
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(peso);
+        System.out.println(altura);
+    }
+}
