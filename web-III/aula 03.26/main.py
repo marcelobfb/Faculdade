a4d1aad9ad8f5e2204903db50b6b39c58f467ad5
marcelobfb <marcelobouchardet@hotmail.com>
2025-03-26 19:59:29 -0300

    up

--- web-III/aula 03.26/main.py
+++ web-III/aula 03.26/main.py
@@ -1,7 +1,7 @@
 from tkinter import*
 from PIL import Image, ImageTk
-
 i=Tk()
+'''
 i.title('Primeira janera')
 # abrir fullscreen:
 i.state('zoomed')
@@ -41,7 +41,13 @@ btn1=Button(i,
 btn1.pack(pady=20)
 
 label = Label(i, bg='black', fg='white', font="Arial 12 bold")
-label.pack()
+label.pack()'''
 
-
+# sistema grid(linha e coluna)
+x1=Label(i,text='Teste1',bg="orange",font="Arial 12 bold")
+x2=Label(i,text='Teste1',bg="red",font="Arial 12 bold")
+x3=Label(i,text='Teste1',bg="purple",font="Arial 12 bold")
+x1.grid(row=0,column=0)
+x2.grid(row=0,column=1)
+x3.grid(row=0,column=2)
 i.mainloop()
