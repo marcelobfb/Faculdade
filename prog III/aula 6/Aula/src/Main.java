1b8c09060bc02ef24e2a94626efe9ccf477167d3
marcelobfb <145417804+marcelobfb@users.noreply.github.com>
2025-03-27 19:39:12 -0300

    up

--- prog III/aula 6/Aula/src/Main.java
+++ prog III/aula 6/Aula/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc=new Scanner(System.in);
+        int[] idades= new int[3];
+        for (int i=0;i<3;i++){
+            System.out.print("Digite idade: ");
+            idades[i]=sc.nextInt();
+        }
+        for (int idade : idades){
+            System.out.println("Idades: "+idade);
+        }
+//        for (int i=0;i<3;i++){
+//            System.out.println("Idades: "+idades[i]);
+//        }
     }
 }
