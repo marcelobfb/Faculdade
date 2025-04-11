08ed3cfe8f0d423f35b9d7722a70c62d1f5ebf5a
marcelobfb <145417804+marcelobfb@users.noreply.github.com>
2025-04-11 18:08:49 -0300

    estudo

--- web-III/Estudo/main.py
+++ web-III/Estudo/main.py
@@ -1,9 +1,17 @@
-# 1) b
-# 2)  
-# 3)
+from tkinter import*
 
-# from tkinter import*
-
-# tk=Tk()
-# tk.mainloop()
+def numero():
+    inteiro=int(input1.get())
+    binario=bin(inteiro)[2:]
+    resp_label.config(text=f"O número transformado em binario é {binario}")
+    
+tk=Tk()
+header=Label(tk,text="Digite um numero para virar binario: ",padx=10,pady=10).pack()
+input1=Entry(tk)
+input1.pack(padx=10,pady=10)
+btn=Button(tk,text="Converter",command=numero)
+btn.pack(padx=10,pady=10)
+resp_label=Label(tk,text=" ",padx=10,pady=10)
+resp_label.pack()
+tk.mainloop()
 
