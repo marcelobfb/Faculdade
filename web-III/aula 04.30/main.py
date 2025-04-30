af3c0394ea5388188edf13ecd4ec1432ff6ad711
Marcelo Bernardes <marcelobouchardet@hotmail.com>
2025-04-30 19:37:31 -0300

    up

--- web-III/aula 04.30/main.py
+++ web-III/aula 04.30/main.py
@@ -1,7 +1,29 @@
 from mysql import connector
 import mysql
 
-conexao = mysql.connector.connect(host='LocalHost', user='root', password='<PASSWORD>')
+conexao = mysql.connector.connect(host='LocalHost', user='root', password='', database='testemarcelo')
 cursor = conexao.cursor()
 
+# criando a base de dados--------------------------------------
+# cursor.execute('create database if not exists testemarcelo')
 
+
+# mostra todos os bancos de dados------------------------------
+# cursor.execute('show databases')
+# for i in cursor:
+#     print(i)
+
+# usando o banco de dados---------------------------------------
+# cursor.execute('use testemarcelo')
+
+# criar tabela--------------------------------------
+# cursor.execute('''create table aluno(
+#     matricula int primary key auto_increment,
+#     nome varchar(30) not null,
+#     idade int(3),
+#     email varchar(50))''')
+
+# mostrar todas as tabelas
+cursor.execute('show tables')
+for i in cursor:
+    print(i)
