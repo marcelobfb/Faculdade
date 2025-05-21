e8a5b537059a15aee24d9815987a07a3a6e094b3
marcelobfb <marcelobouchardet@hotmail.com>
2025-05-21 19:51:41 -0300

    up

--- web-III/aula 05.21/main.py
+++ web-III/aula 05.21/main.py
@@ -1,3 +1,7 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
 class Aluno:
     def __init__(self, nome, idade, curso):
         self.nome = nome
@@ -35,58 +39,59 @@ class Aluno:
         else:
             print(f'{self.nome} foi reprovado.')
 
-    def menu():
-        aluno = None
+
+    # def menu():
+    #     aluno = None
         
-        while True:
-            print('\n===== Menu =====')
-            print('1- Cadastrar Aluno')
-            print('2- Adicionar nota do aluno')
-            print('3- Média/aprovação')
-            print('4- Mostrar dados do aluno')
-            print('5- Sair')
+    #     while True:
+    #         print('\n===== Menu =====')
+    #         print('1- Cadastrar Aluno')
+    #         print('2- Adicionar nota do aluno')
+    #         print('3- Média/aprovação')
+    #         print('4- Mostrar dados do aluno')
+    #         print('5- Sair')
 
-            opcao = input('Escolha a opção desejada (1-5): ').strip()
+    #         opcao = input('Escolha a opção desejada (1-5): ').strip()
 
-            if not opcao:
-                print('Entrada vazia. Digite uma opção.')
-                continue
-            
-            if opcao == '1':
-                nome = input('Digite o nome do aluno: ')
-                idade = int(input('Digite a idade do aluno: '))
-                curso = input('Digite o curso do aluno: ')
-                aluno = Aluno(nome, idade, curso)
-                print('Aluno cadastrado com sucesso.')
+    #         if not opcao:
+    #             print('Entrada vazia. Digite uma opção.')
+    #             continue
 
-            elif opcao == '2':
-                if aluno:
-                    try:
-                        nota = float(input('Digite a nota do aluno: '))
-                        aluno.add_nota(nota)
-                    except ValueError:
-                        print('Nota inválida. Insira uma nota entre 0 a 10.')
-                else:
-                    print('Cadastre um aluno primeiro (opção 1).')
+    #         if opcao == '1':
+    #             nome = input('Digite o nome do aluno: ')
+    #             idade = int(input('Digite a idade do aluno: '))
+    #             curso = input('Digite o curso do aluno: ')
+    #             aluno = Aluno(nome, idade, curso)
+    #             print('Aluno cadastrado com sucesso.')
 
-            elif opcao == '3': 
-                if aluno:
-                    aluno.verificar_aprovacao()
-                else:
-                    print('Nenhum aluno cadastrado.')
+    #         elif opcao == '2':
+    #             if aluno:
+    #                 try:
+    #                     nota = float(input('Digite a nota do aluno: '))
+    #                     aluno.add_nota(nota)
+    #                 except ValueError:
+    #                     print('Nota inválida. Insira uma nota entre 0 a 10.')
+    #             else:
+    #                 print('Cadastre um aluno primeiro (opção 1).')
 
-            elif opcao == '4': 
-                if aluno:
-                    aluno.apresentar()
-                    print(f'Média atual: {aluno.calcular_media():.2f}')
-                else:
-                    print('Nenhum aluno cadastrado.')
+    #         elif opcao == '3': 
+    #             if aluno:
+    #                 aluno.verificar_aprovacao()
+    #             else:
+    #                 print('Nenhum aluno cadastrado.')
 
-            elif opcao == '5':  
-                print('Saindo...')
-                break
-            else:
-                print('Opção inválida. Tente novamente.')
+    #         elif opcao == '4': 
+    #             if aluno:
+    #                 aluno.apresentar()
+    #                 print(f'Média atual: {aluno.calcular_media():.2f}')
+    #             else:
+    #                 print('Nenhum aluno cadastrado.')
+
+    #         elif opcao == '5':  
+    #             print('Saindo...')
+    #             break
+    #         else:
+    #             print('Opção inválida. Tente novamente.')
 
 # aluno1 = Aluno('Thereza', 21, 'Python')
 # aluno1.apresentar()
@@ -97,4 +102,60 @@ class Aluno:
 # aluno1.verificar_aprovacao()
 # aluno1.maior_idade()
 
-Aluno.menu()
+# Aluno.menu()
+class App:
+    def __init__(self,root):
+        self.root = root
+        self.root.title('Gerenciador de Aluno')
+        self.aluno = None
+        self.create__widgets()
+        
+    def create_widgets(self):
+        tk.Label(self.root, text='Aluno').grid(row=0,column=0)
+        self.nome_entry=tk.Entry(self.root)
+        self.nome_entry.grid(row=0,column=1)
+        
+        tk.Label(self.root, text='Idade').grid(row=1,column=0)
+        self.idade_entry=tk.Entry(self.root)
+        self.idade_entry.grid(row=1,column=1)
+        
+        tk.Label(self.root, text='Curso').grid(row=2,column=0)
+        self.curso_entry=tk.Entry(self.root)
+        self.curso_entry.grid(row=2,column=1)
+        
+        tk.Button(self.root,text='Cadastrar Aluno',command=self.cadastrar_aluno).grid(row=3,colspan=2,pady=10)
+        
+        tk.Label(self.root, text='Nota').grid(row=4,column=0)
+        self.nota_entry=tk.Entry(self.root)
+        self.nota_entry.grid(row=4,column=1)
+        
+        tk.Button(self.root,text='Adicionar Nota',command=self.adicionar_nota).grid(row=5,colspan=2,pady=10)
+        tk.Button(self.root,text='Media/Situação',command=self.mostrar_media).grid(row=6,colspan=2,pady=10)
+        tk.Button(self.root,text='Mostrar Dados',command=self.mostrar_dados).grid(row=7,colspan=2,pady=10)
+        
+    def cadastrar_aluno(self):
+        nome=self.nome_entry.get().strip()
+        idade=self.idade_entry.get().strip()
+        curso=self.curso_entry.get().strip()
+        
+        if nome and idade.isdigit() and curso:
+            self.aluno=Aluno(nome,idade,curso)
+            messagebox.showinfo('Cadastro Aluno','Aluno cadastrado com sucesso.')
+        else:
+            messagebox.showwarning('Erro','Erro! Preencha todos os campos.')
+            
+    def adicionar_nota(self):
+        if not self.aluno:
+            messagebox.showwarning('Erro','Erro! Cadastre um aluno primeiro')
+            return
+        
+        nota=self.nota_entry.get().strip()
+        try:
+            nota=float(nota)
+            if self.aluno.adicionar_nota(nota):
+                messagebox.showinfo('Nota','Nota cadastrada com sucesso.')
+            else:
+                messagebox.showwarning('Erro','Nota entre 0 e 10')
+        except ValueError:
+            messagebox.showwarning('Erro','Digite um numero valido')
+            
