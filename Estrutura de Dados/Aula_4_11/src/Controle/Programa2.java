c9096449a42457f5b560994600be431259d4fa9c
marcelobfb <marcelobouchardet@hotmail.com>
2025-11-04 19:54:10 -0300

    up

---
+++ Estrutura de Dados/Aula_4_11/src/Controle/Programa2.java
@@ -0,0 +1,33 @@
+package Controle;
+
+import java.util.Scanner;
+
+public class Programa2 {
+	
+	public static int pesquisar(int[] numeros, int num) {
+		for(int i = 0; i < numeros.length; i++) {
+			if(numeros[i] == num) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int[] numeros = {3, 17, 25, 38, 68, 79, 94, 1, 5, 23, 26, 42, 70, 83};
+		
+		System.out.print("Digite um numero para pesquisar: ");
+		int num = input.nextInt();
+		
+		int indiceEncontrado = pesquisar(numeros, num);
+		
+		if (indiceEncontrado != -1) {
+            System.out.println("O número " + num + " foi encontrado no índice: " + indiceEncontrado);
+        } else {
+            System.out.println("O número " + num + " não foi encontrado no array.");
+        }
+		
+		input.close();
+	}
+}
